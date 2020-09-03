a58bf60040434cc5dbd4a30640303c655002d173
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-03 17:19:08 +0200

    WIP dropdown menu all'ohver

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+  // apro il menu all'hover sul link
+  $(".open-menu").on("mouseover",
+    function(){
+      $(".dropdown-menu").show()
+    }
+  );
+
+  $(".open-menu").mouseout(
+    function(){
+      $(".dropdown-menu").hide()
+    }
+  );
 
 });
